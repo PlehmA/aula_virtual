092eaff0b5ca70d353daa71382e2f2ee8aa31a1e
Andres Plehm <polachgg@gmail.com>
2016-09-17 15:18:50 -0300

    Modificaciones! Cambios simples del login y register, hardcodeando archivos de configuracion y base de datos.

--- login.php
+++ login.php
@@ -2,9 +2,9 @@
 session_start();//crea una sesión para ser usada mediante una petición GET o POST, o pasado por una cookie y la sentencia include_once es la usaremos para incluir el archivo de conexión a la base de datos que creamos anteriormente.
 include_once "conexion.php"; 
 /*Creamos el formulario con el campo de Usuario que se llamara $_POST['usuario'] y 2 campos para la clave y uno mas para confirmar si escribió bien la clave, se llamaran $_POST['password'] y $_POST['repassword'] respectivamente, procedemos a escribir el codigo que procesara y validara lo que el usuario ingrese:*/
-if(isset($_POST['enviar']))//para saber si el botón registrar fue presionado. 
+if(isset($_POST['enviar']))//para saber si el boton registrar fue presionado. 
 { 
-    if($_POST['usuario'] == '' or $_POST['password'] == '' or $_POST['repassword'] == '' or $POST['email'] == '')
+    if($_POST['usuario'] == '' or $_POST['password'] == '' or $_POST['repassword'] == '' or $_POST['email'] == '')
     { 
         echo 'Por favor llene todos los campos.';//Si los campos están vacíos muestra el siguiente mensaje, caso contrario sigue el siguiente codigo.
     } 
@@ -12,11 +12,11 @@ if(isset($_POST['enviar']))//para saber si el botón registrar fue presionado.
     { 
         $sql = 'SELECT * FROM usuarios'; 
         $rec = mysql_query($sql); 
-        $verificar_usuario = 0;//Creamos la variable $verificar_usuario que empieza con el valor 0 y si la condición que verifica el usuario(abajo), entonces la variable toma el valor de 1 que quiere decir que ya existe ese nombre de usuario por lo tanto no se puede registrar
+        $verificar_usuario = 0;//Creamos la variable $verificar_usuario que empieza con el valor 0 y si la condicion que verifica el usuario(abajo), entonces la variable toma el valor de 1 que quiere decir que ya existe ese nombre de usuario por lo tanto no se puede registrar
   
         while($result = mysql_fetch_object($rec)) 
         { 
-            if($result->usuario == $_POST['usuario']) //Esta condición verifica si ya existe el usuario 
+            if($result->usuario == $_POST['usuario']) //Esta condici�n verifica si ya existe el usuario 
             { 
                 $verificar_usuario = 1; 
             } 
@@ -27,8 +27,9 @@ if(isset($_POST['enviar']))//para saber si el botón registrar fue presionado.
             if($_POST['password'] == $_POST['repassword'])//Si los campos son iguales, continua el registro y caso contrario saldrá un mensaje de error.
             { 
                 $usuario = $_POST['usuario']; 
-                $password = $_POST['password']; 
-                $sql = "INSERT INTO usuarios (usuario,password) VALUES ('$usuario','$password')";//Se insertan los datos a la base de datos y el usuario ya fue registrado con exito.
+                $password = $_POST['password'];
+                $email = $_POST['email'];
+                $sql = "INSERT INTO usuarios (usuario,password,email) VALUES ('$usuario','$password','$email')";//Se insertan los datos a la base de datos y el usuario ya fue registrado con exito.
                 mysql_query($sql); 
   
                 echo 'Usted se ha registrado correctamente.'; 
